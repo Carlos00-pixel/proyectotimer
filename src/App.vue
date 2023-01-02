6e8ab52d1642e384656deb0fbb442d6bbe0cdd70
NazarRom <nazarrom321@gmail.com>
2023-01-02 22:15:20 +0100

    Pruebas

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <inicio-component></inicio-component>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import InicioComponent from './components/InicioComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    InicioComponent
   }
 }
 </script>
